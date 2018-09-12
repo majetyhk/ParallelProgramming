e809b5922bd40187835ac962c48969d974518478
Hari Krishna Majety <majetyhk@gmail.com>
2018-09-12 01:33:44 -0400

    Added Comments to p2_mpi

--- HW1/p3/p2_mpi.c
+++ HW1/p3/p2_mpi.c
@@ -30,34 +30,32 @@ int main (int argc, char *argv[])
         int   numproc, rank, len, source, dest;
         char *message;
         int tag;
-        MPI_Status status,status2;
-  //      struct timeval t1, t2;
-        double *elapsedTime;
-        int startValue;
-        int endValue;
-        int NGRID,communication,mpi_gather;
+        MPI_Status status;                    // Status for Receive Calls
+        int startValue, endValue;             // For Defining Processes End Grid Points
+        int NGRID,communication,mpi_gather;   // For storing the Argument Values
         MPI_Datatype stype;
-        MPI_Request req[10]; 
-        struct timeval t1,t2;
-        int *displs,*rcounts; 
-        double *finalDerivBuff;
-        double *tyc;
+        MPI_Request req[10];                  // Request Handlers for Non-Blocking Calls
+        struct timeval t1,t2;                 // Time Variables for Performance Monitoring  
+        int *displs,*rcounts;                 // Displacement and Count Buffer used under MPI_Gather
+        double *finalDerivBuff;               // Final Derivate Buffer Used under MPI_Gather
+
+        // Assigning the argument Values
         if(argc > 1)
         {
             NGRID = atoi(argv[1]);
         }
             if(argc>2){
             communication = atoi(argv[2]);
-            //printf("NGRID: %d Comm Value %d\n", NGRID, communication);
         }
          if(argc > 3){
-		mpi_gather = atoi(argv[3]);
-	    }
+        mpi_gather = atoi(argv[3]);
+        }
         else 
         {
                 printf("Please specify the number of grid points.\n");
                 exit(0);
         }
+
         //loop index
         int         i;
 
@@ -97,7 +95,6 @@ int main (int argc, char *argv[])
         for (i = 1; i <= NGRID ; i++)
         {
                 xc[i] = XI + (XF - XI) * (double)(i - 1)/(double)(NGRID - 1);
-                //printf("XC[%d]: %lf\n ",i,xc[i]);
         }
         //step size and boundary points
         dx = xc[2] - xc[1];
@@ -106,7 +103,7 @@ int main (int argc, char *argv[])
 
         
 
-        //define the function
+        //Allocating the Grid Points(Start & End) based on the rank of the Process
         startValue = rank*pointsPerProcess+1;
         if(rank+1<numproc){
             endValue = startValue + pointsPerProcess-1;
@@ -115,21 +112,15 @@ int main (int argc, char *argv[])
             endValue = NGRID;
         }
         int totalPoints = endValue-startValue+1;
-       // printf("Rank: %d, Start: %d , End: %d\n",rank,startValue,endValue);
-        //endValue = pointsPerProcess+(remPoints>rank)?1:0;
 
         //allocate function arrays
         yc  =   (double*) malloc((NGRID + 2) * sizeof(double));
         dyc =   (double*) malloc((NGRID + 2) * sizeof(double));
 
-        //allocate function arrays
-        //tyc  =   (double*) malloc((totalPoints + 2) * sizeof(double));
-        //tdyc =   (double*) malloc((totalPoints + 2) * sizeof(double));
-
+        //Calculating the fn(x) values and allocating it to yc
         for( i = startValue; i <= endValue ; i++ )
         {
                 yc[i] = fn(xc[i]);
-                //tyc[i-startValue+1] = yc[i];
         }
 
         //set boundary values
@@ -140,12 +131,14 @@ int main (int argc, char *argv[])
         //should be set
         yc[0] = fn(xc[0]);
         yc[imax + 1] = fn(xc[NGRID + 1]);
-        gettimeofday(&t1,NULL);
+       
+        gettimeofday(&t1,NULL);  // Getting Time for Performance Analysis
 
-        if(communication == 0)
+
+
+        if(communication == 0)              // If Border Communication is to be done with Blocking Calls(Communication Logic in README)
         {
-            //printf("Entered into Comm value 0 case\n");
-                if(rank%2==0)
+                if(rank%2==0)               
                 {
                     tag = rank;
                     if(rank+1<numproc){
@@ -177,9 +170,8 @@ int main (int argc, char *argv[])
                     
                 }
         }
-        else
+        else                                // If Border Communication is to be done with Non-Blocking Calls(Communication Logic in README)
         {
-            //printf("Entered into Comm value 1 case with Rank %d\n",rank);
                 if(rank%2==0){
                     tag = rank;
                     if(rank+1<numproc){
@@ -195,7 +187,7 @@ int main (int argc, char *argv[])
                         MPI_Isend(yc + startValue,1,MPI_DOUBLE,rank-1,tag,MPI_COMM_WORLD,&req[3]);
                     }
                 }
-                else{
+                else{           
                     tag = rank-1;
                     MPI_Isend(yc + startValue,1,MPI_DOUBLE,rank-1,tag,MPI_COMM_WORLD,&req[4]);
                     if(rank+1<numproc){
@@ -210,9 +202,8 @@ int main (int argc, char *argv[])
                     
                 }
         }
-        if(communication !=0)
+        if(communication !=0)           // Incase of Non-Blocking Calls, Waiting for all the processes to complete communication 
         {
-            //printf("Stopping Here before proceeding with Rank %d\n",rank);
             if(rank%2==0)
             {
                 tag = rank;
@@ -235,81 +226,86 @@ int main (int argc, char *argv[])
                 }
         }
 
-                 //compute the derivative using first-order finite differencing       
+        //compute the derivative using first-order finite differencing       
         for (i = startValue; i <= endValue; i++)
         {
             dyc[i] = (yc[i + 1] - yc[i - 1])/(2.0 * dx);
         }
-        if(mpi_gather==1 && communication == 0)
+        if(communication == 0 && mpi_gather==1)         // Incase of Manual Gather Using Blocking Calls                    
         {
-            //printf("\n******** manual gather *********\n");
-        	if(rank>0){
-        	    MPI_Send(yc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD);
-        	    MPI_Send(dyc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD);
-        	}
-            if(rank==0) 
+            if(rank>0){                                 // All Processes Sending the values over to Rank 0 Process
+                MPI_Send(yc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD);
+                MPI_Send(dyc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD);
+            }
+            if(rank==0)                                  //Allocating Arrays to store all the values to be received from all the processes
             {
-        	    double* all_arrays = (double*)malloc(NGRID*sizeof(double));
-        	    double* all_arrays_yc = (double*)malloc(NGRID*sizeof(double));
-                for(int i=startValue;i<=endValue;i++)
-        	    {
-        		    all_arrays[i]=dyc[i];
-        		    all_arrays_yc[i]=yc[i];
-        	    }
+                double* all_arrays = (double*)malloc(NGRID*sizeof(double));
+                double* all_arrays_yc = (double*)malloc(NGRID*sizeof(double));
+                for(int i=startValue;i<=endValue;i++)   // Storing It's Own(Rank 0) computed values into the main array
+                {
+                    all_arrays[i]=dyc[i];
+                    all_arrays_yc[i]=yc[i];
+                }
 
-        	    for (int i=1; i<numproc-1; i++)
+                for (int i=1; i<numproc-1; i++)         // Issuing Recv Calls for all the processes except the last one
                 {
-        		    MPI_Recv(&all_arrays_yc[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&status);
-        		    MPI_Recv(&all_arrays[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&status);
-        	    }
+                    MPI_Recv(&all_arrays_yc[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&status);
+                    MPI_Recv(&all_arrays[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&status);
+                }
+                                                        // Receive calls for the last Process since it has different grid points than all.
                 MPI_Recv(&all_arrays_yc[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&status);
-        		MPI_Recv(&all_arrays[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&status);
-        	    print_function_data(NGRID, &xc[1], &all_arrays_yc[1], &all_arrays[1]);
-        	}
+                MPI_Recv(&all_arrays[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&status);
+                print_function_data(NGRID, &xc[1], &all_arrays_yc[1], &all_arrays[1]);
+            }
         }
-        else if(mpi_gather==1 && communication == 1)
+        else if(mpi_gather==1 && communication == 1)           // Incase of Manual Gather using Non-Blocking Calls
         {
-           //printf("\n******** manual gather with non blocking function calls *********\n");
-        	if(rank>0){
-        	    MPI_Isend(yc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD,&req[8]);
-        	    MPI_Isend(dyc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD,&req[9]);
-        	}
-            if(rank==0) 
+            if(rank>0)                                        // All Processes Sending the values over to Rank 0 Process
             {
-        	    double* all_arrays = (double*)malloc(NGRID*sizeof(double));
-        	    double* all_arrays_yc = (double*)malloc(NGRID*sizeof(double));
-                for(int i=startValue;i<=endValue;i++)
-        	    {
-        		    all_arrays[i]=dyc[i];
-        		    all_arrays_yc[i]=yc[i];
-        	    }
-                MPI_Request req2[numproc], req3[numproc];
-        	    for (int i=1; i<numproc-1; i++)
+                MPI_Isend(yc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD,&req[8]);
+                MPI_Isend(dyc+startValue,endValue-startValue+1,MPI_DOUBLE,0,50,MPI_COMM_WORLD,&req[9]);
+            }
+
+            if(rank==0)                                        //Allocating Arrays to store all the values to be received from all the processes
+            {
+                double* all_arrays = (double*)malloc(NGRID*sizeof(double));
+                double* all_arrays_yc = (double*)malloc(NGRID*sizeof(double));
+
+                for(int i=startValue;i<=endValue;i++)           // Storing It's Own(Rank 0) computed values into the main array
                 {
-        		    MPI_Irecv(&all_arrays_yc[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&req2[i]);
-        		    MPI_Irecv(&all_arrays[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&req3[i]);
-        	    }
+                    all_arrays[i]=dyc[i];
+                    all_arrays_yc[i]=yc[i];
+                }
+                MPI_Request req2[numproc], req3[numproc];
+                for (int i=1; i<numproc-1; i++)                 // Issuing Irecv Calls for all the processes except the last one
+                {
+                    MPI_Irecv(&all_arrays_yc[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&req2[i]);
+                    MPI_Irecv(&all_arrays[endValue+1+((i-1)*(endValue-startValue+1))],endValue-startValue+1,MPI_DOUBLE,i,50,MPI_COMM_WORLD,&req3[i]);
+                }
+
+                                                                // Receive calls for the last Process since it has different grid points than all. 
                 MPI_Irecv(&all_arrays_yc[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&req2[numproc-1]);
-        		MPI_Irecv(&all_arrays[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&req3[numproc-1]);
-        	    
-                for(int i=1; i<numproc;i++)
+                MPI_Irecv(&all_arrays[((numproc-1)*(endValue-startValue+1))+1],NGRID-((numproc-1)*startValue)+1,MPI_DOUBLE,numproc-1,50,MPI_COMM_WORLD,&req3[numproc-1]);
+                
+                for(int i=1; i<numproc;i++)                     // Waiting for all the Processes to complete the communications.
                 {
                     MPI_Wait(&req2[i],&status);
                     MPI_Wait(&req3[i],&status);
                 }
-        	    print_function_data(NGRID, &xc[1], &all_arrays_yc[1], &all_arrays[1]);
-        	} 
+                print_function_data(NGRID, &xc[1], &all_arrays_yc[1], &all_arrays[1]);
+            } 
         }
-        else
+        else                        // Incase of MPI_Gather
         {
-            //printf("\n********* MPI_GATHER *******\n");
+            
             if(rank==0)
             {
-                finalDerivBuff = (double*) malloc((NGRID + 2) * sizeof(double));
-                tyc = (double*) malloc((NGRID + 2) * sizeof(double));
-                displs = (int *)malloc(numproc*sizeof(int)); 
-                rcounts = (int *)malloc(numproc*sizeof(int));
-                for (i=0; i<numproc; ++i) 
+                finalDerivBuff = (double*) malloc((NGRID + 2) * sizeof(double));       // Final Buffer storing all the derivative values
+                tyc = (double*) malloc((NGRID + 2) * sizeof(double));                  // Buffer to store all the Function(y) values
+                displs = (int *)malloc(numproc*sizeof(int));                           // Displacement Array for the buffer, to know where to place the gathered values 
+                rcounts = (int *)malloc(numproc*sizeof(int));                          // Count to define how many values to be gathered from respective process
+                
+                for (i=0; i<numproc; ++i)                           // Defining the Displacement and Count Array Values
                 { 
                     displs[i] = i*pointsPerProcess;
                     if(i==numproc-1)
@@ -322,18 +318,21 @@ int main (int argc, char *argv[])
                     }
                 }
             }  
-            double *k;
+            double *k;                                            
             k=dyc+startValue;
+            // Gathering all the values to the Rank 0 Process
             MPI_Gatherv(dyc+startValue,totalPoints,MPI_DOUBLE,finalDerivBuff,rcounts,displs,MPI_DOUBLE, 0,MPI_COMM_WORLD);
             MPI_Gatherv(yc+startValue,totalPoints,MPI_DOUBLE,tyc,rcounts,displs,MPI_DOUBLE,0,MPI_COMM_WORLD);
             if(rank==0)
             {
                 print_function_data(NGRID, &xc[1], &tyc[0], &finalDerivBuff[0]);
             }
-        }  
+        }
+
         gettimeofday(&t2,NULL);
         double timeInterval = ((t2.tv_sec - t1.tv_sec) * 1000.0);
         timeInterval += (t2.tv_usec - t1.tv_usec) / 1000.0;
+       
         if(rank==0){
             printf("\nTime taken = %lf\n",timeInterval);
         }
@@ -360,7 +359,6 @@ void print_function_data(int np, double *x, double *y, double *dydx)
 
         for(i = 0; i < np; i++)
         {
-//                printf("%f %f %f\n", x[i], y[i], dydx[i]);
             fprintf(fp, "%f, %f, %f\n", x[i], y[i], dydx[i]);
         }
 
